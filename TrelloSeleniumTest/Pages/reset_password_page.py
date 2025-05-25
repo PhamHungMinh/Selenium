40f2a81c292a42423d42791e595e332012ea9ad1
minh <0306221442@caothang.edu.vn>
2025-05-25 22:52:55 +0700

    Update

---
+++ TrelloSeleniumTest/Pages/reset_password_page.py
@@ -0,0 +1,28 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+class PasswordResetPage:
+    # Locators
+    EMAIL_FIELD = (By.ID, "email")
+    SUBMIT_BUTTON = (By.ID, "reset-password-email-submit")
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.driver.maximize_window()
+        self.wait = WebDriverWait(self.driver, 20)
+
+    def open_reset_page(self):
+        self.driver.get("https://id.atlassian.com/login/resetpassword?application=trello")
+        return self
+
+    def enter_email(self, email):
+        email_field = self.wait.until(EC.presence_of_element_located(self.EMAIL_FIELD))
+        email_field.clear()
+        email_field.send_keys(email)
+        return self
+
+    def click_submit(self):
+        self.wait.until(EC.element_to_be_clickable(self.SUBMIT_BUTTON)).click()
+        return self
---
+++ TrelloSeleniumTest/Tests/Test_password_reset.py
@@ -0,0 +1,30 @@
+import time
+
+import pytest
+import sys
+import os
+
+from Selenium.TrelloSeleniumTest.Pages.reset_password_page import PasswordResetPage
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from TrelloSeleniumTest.Pages.reset_password_page import PasswordResetPage
+
+
+class TestPasswordReset:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        self.password_page = PasswordResetPage(self.driver)
+        yield
+        self.driver.quit()
+
+    def test_successful_password_reset(self):
+        (self.password_page.open_reset_page()
+         .enter_email("nghiatrong4554@gmail.com")
+         .click_submit())
+        time.sleep(10)
+
